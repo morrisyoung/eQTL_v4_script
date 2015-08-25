cb1dc9987e93dbcb7f65345bb5c8c36ed04b12e0
morrisyoung <sdmorrisys@gmail.com>
2015-08-25 10:18:57 -0400

    update

--- gene_tss_prepare.py
+++ gene_tss_prepare.py
@@ -3,6 +3,7 @@
 
 if __name__ == '__main__':
 
+	## get the chr and tss for all genes (including X, Y and MT genes)
 	file = open("../gencode.v18.genes.patched_contigs.gtf", 'r')
 	file1 = open("../gencode.v18.genes.patched_contigs.gtf_gene_tss", 'w')
 
@@ -26,5 +27,29 @@ if __name__ == '__main__':
 		if type == 'transcript':
 			file1.write(gene + '\t' + chr + '\t' + tss + '\n')
 
+	file.close()
+	file1.close()
+
+
+
+
+
+
+	## get the list of all X, Y, MT genes
+	file = open("../gencode.v18.genes.patched_contigs.gtf_gene_tss", 'r')
+	file1 = open("../gencode.v18.genes.patched_contigs.gtf_gene_xymt", 'w')
+
+	while 1:
+		line = (file.readline()).strip()
+		if not line:
+			break
+
+		line = line.split('\t')
+		gene = line[0]
+		chr = line[1]
+		tss = line[2]
+
+		if chr == 'X' or chr == 'Y' or chr == 'MT':
+			file1.write(gene + '\n')
 	file.close()
 	file1.close()
