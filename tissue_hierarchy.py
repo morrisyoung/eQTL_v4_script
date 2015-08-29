d25e9b1dadc6d11b6738d54292dbb3f49aa8d76b
morrisyoung <sdmorrisys@gmail.com>
2015-08-29 17:45:28 -0400

    update

--- tissue_hierarchy.py
+++ tissue_hierarchy.py
@@ -1,9 +1,12 @@
 ## to do the tissue hierarchical clustering on all 17 etissues (with both training samples and testing samples)
+import numpy as np
+import scipy
 
 
 if __name__ == '__main__':
 
 
+	'''
 	##==================================== == == == == == == == ==
 	file = open("../phs000424.v4.pht002743.v4.p1.c1.GTEx_Sample_Attributes.GRU.txt_tissue_type", 'r')
 	sample_tissue_map = {}
@@ -92,3 +95,45 @@ if __name__ == '__main__':
 		print tissue,
 		print len(tissue_expression_ave[tissue])
 
+
+
+	##==================================== == == == == == == == ==
+	file = open("../GTEx_Data_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct_processed_2_gene_normalized_tissue_mean", 'w')
+	for tissue in tissue_expression_ave:
+		file.write(tissue + '\t')
+		for rpkm in tissue_expression_ave[tissue]:
+			file.write(str(rpkm) + '\t')
+		file.write('\n')
+	print "saving done!"
+	'''
+
+
+	file = open("../GTEx_Data_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct_processed_2_gene_normalized_tissue_mean", 'r')
+	rep = {}
+	dimension = 0
+	tissue_list = []
+	while 1:
+		line = (file.readline()).strip()
+		if not line:
+			break
+
+		line = line.split('\t')
+		tissue = line[0]
+		expression = map(lambda x: float(x), line[1:])
+		rep[tissue] = expression
+		dimension = len(rep[tissue])
+		tissue_list.append(tissue)
+	file.close()
+
+	array = np.ndarray(shape=(len(rep), dimension), dtype=float)
+	for i in range(len(tissue_list)):
+		tissue = tissue_list[i]
+		for j in range(len(rep[tissue])):
+			rpkm = rep[tissue][j]
+			array[i][j] = rpkm
+
+
+
+	Y = scipy.spatial.distance.pdist(array, 'euclidean')
+	Z = scipy.cluster.hierarchy.linkage(Y, method='single', metric='euclidean')
+	scipy.cluster.hierarchy.dendrogram(Z)
