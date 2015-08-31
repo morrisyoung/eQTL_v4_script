b7e5e1fc91b7de62ada7acde43fa9753baf997ee
morrisyoung <sdmorrisys@gmail.com>
2015-08-31 14:41:06 -0400

    update

--- README.md
+++ README.md
@@ -4,7 +4,28 @@ All the data files should be in the upper folder of this directory, for appropri
 
 The order of these scripts are:
 
-1. 
+** Genotype:
+
+1. genotype\_ld\_prune.py
+
+** Genotype backup (these should happen before above script):
+
+1. script.py
+
+2. post\_prune.py
+
+3. [to-do] dosage\_qc\_chunk.py
+
+
+** Expression:
+
+eSample\_partition.py
+
+gene\_preprocess.py
+
+sample\_tissue\_preprocess.py
+
+x. tissue\_hierarchy.py
 
 
 
@@ -13,4 +34,37 @@ The order of these scripts are:
 
 Other scripts (not necessary in order):
 
-1. GTEx\_beta\_extract.py
+1. gene\_tss\_prepare.py
+
+2. GTEx\_beta\_extract.py
+
+
+
+
+The pipeline for genotype QC and LD pruning:
+
+As I may use different LD threshold (currently 0.5 for R^2) and the association threshold (currently 0.5 for R^2, the same with previous one) in the future, and there is hard drive usage issue, I record the procedure here.
+
+(there are two scripts used here: “script.py" is the general script for processing all the chromosomes, following the below procedure; “post\_prune.py” is the one used for reversing the associations between pruned SNPs and the un-pruned SNPs; the two scripts are all in C2B2 “/ifs/scratch/c2b2/ip\_lab/sy2515/GTEx/data.v.5/44712/PhenoGenotypeFiles/RootStudyConsentSet\_phs000424.GTEx.v5.p1.c1.GRU/GenotypeFiles/phg000219.v4.GTEx\_Pilot\_Imputation.genotype-imputed-data.c1\_ld\_qc/")
+
+tar zxvf “all.SNPs.tgz”
+mkdir post\_prune
+for all the chromosome “X”, do the following (step#04 — step#16):
+tar zxvf “chrX.all.tgz”
+cp chrX.tfam "chrX.tfam"
+[QC] ./plink —tfile “chrX” —exclude “chrX.info4.maf05.exclusion.snplist.txt” —make-bed
+[pruning] ./plink —bfile plink —indep-pairwise 50kb 5 0.5 (0.5 may possibly be adjusted into other values)
+[LD statistics] ./plink --bfile plink --r2 --ld-snp-list plink.prune.out --ld-window-kb 50 --ld-window 99999 --ld-window-r2 0.5 (0.5 can be adjusted into other values)
+[LD statistics further] python post\_prune.py
+mv plink.prune.in ./post\_prune/chr”X”.prune.in
+mv plink.prune.out ./post\_prune/chr”X”.prune.out
+mv plink.ld ./post\_prune/chr”X”.ld
+mv chr”X".post\_prune.txt ./post\_prune/
+rm chr22.*
+rm data_imputed.*
+rm plink.*
+
+Then, I need to do the following procedure to get the genotype data (dosage) we can use in learning.
+
+remove QC-ed SNPs from dosage file (simple; to be done with Python, in “/ifs/scratch/c2b2/ip_lab/sy2515/GTEx/data.v.5/44712/PhenoGenotypeFiles/RootStudyConsentSet_phs000424.GTEx.v5.p1.c1.GRU/GenotypeFiles/phg000219.v4.GTEx_Pilot_Imputation.genotype-imputed-data.c1_dosage_qc”)
+use the pruned SNP list to calculate the prior information for all un-pruned SNPs (if necessary), and get the potential for all un-pruned SNPs (will do with Python code; find the scheme first)
---
+++ genotype_ld_prune.py
@@ -0,0 +1,121 @@
+## for doing the data qc for imputed data, with the most strict standard
+
+## the input: 1. chunked dosage data (by chromosome and individual); 2. the pruned.in list from the PLINK pruning program
+## the output: chunked dosage data (by chromosome and individual) with only un-pruned data in
+
+## TODO for new dataset: the individual ID may contain more than 9 characters
+
+
+
+
+if __name__ == '__main__':
+
+
+
+	##========= get 185 individuals first ===========
+	file = open("../GTEx_5M_185_Dec2012.Eigenvectors.txt", 'r')
+	file.readline()
+	rep_individual = {}
+	while 1:
+		line = (file.readline()).strip()
+		if not line:
+			break
+
+		individual = line[0:9]
+		rep_individual[individual] = 1
+	file.close()
+
+
+
+	for chr in range(1, 23):
+
+
+
+		##================ get all SNPs after QC =================
+		rep_snp = {}
+
+		file = open("../genotype_185_dosage_matrix_qc/post_prune/chr" + str(chr) + ".prune.in", 'r')
+		while 1:
+			line = (file.readline()).strip()
+			if not line:
+				break
+
+			snp = line
+			rep_snp[snp] = 1
+		file.close()
+
+		print "working on chr#",
+		print chr,
+		print "and there are",
+		print len(rep_snp),
+		print "un-pruned SNPs."
+
+
+		##========== get the positions of all SNPs left after QC ==========
+		file = open("../genotype_185_dosage_matrix_qc/chr" + str(chr) + "/SNP_info.txt", 'r')
+		rep_index = {}  # record the positions of all SNPs after QC
+		index = 0
+		while 1:
+			line = (file.readline()).strip()
+			index += 1
+			if not line:
+				break
+
+			line = line.split(" ")
+			snp = line[0]
+			if snp in rep_snp:
+				rep_index[index] = 1
+
+		file.close()
+
+
+		##========== re-write all dosage file ============
+		for individual in rep_individual:
+			filename = "../genotype_185_dosage_matrix_qc/chr" + str(chr) + "/SNP_dosage_" + individual + ".txt"
+
+			## go through all the snps for this individual
+			file = open(filename, 'r')
+			dosage_list = []
+			index = 0
+			while 1:
+				line = (file.readline()).strip()
+				index += 1
+				if not line:
+					break
+
+				dosage = line
+				if index in rep_index:
+					dosage_list.append(dosage)
+			file.close()
+
+			## write the list to the file
+			file = open(filename, 'w')
+			for dosage in dosage_list:
+				file.write(dosage + '\n')
+			file.close()
+
+
+
+		##========== re-write all SNP_info.txt files ============
+		filename = "../genotype_185_dosage_matrix_qc/chr" + str(chr) + "/SNP_info.txt"
+
+		## go through all the snps for this individual
+		file = open(filename, 'r')
+		info_list = []
+		index = 0
+		while 1:
+			line = (file.readline()).strip()
+			index += 1
+			if not line:
+				break
+
+			info = line
+			if index in rep_index:
+				info_list.append(info)
+		file.close()
+
+		## write the list to the file
+		file = open(filename, 'w')
+		for info in info_list:
+			file.write(info + '\n')
+		file.close()
