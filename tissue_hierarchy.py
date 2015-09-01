ab065de89c52330de754d11e7f5a97f79e89ea05
morrisyoung <sdmorrisys@gmail.com>
2015-08-31 20:12:07 -0400

    update

--- tissue_hierarchy.py
+++ tissue_hierarchy.py
@@ -3,7 +3,46 @@ import numpy as np
 import scipy
 from scipy import spatial
 from scipy import cluster
-from matplotlib.pyplot import show
+from matplotlib import pyplot as plt
+import dendropy
+from dendropy.calculate import treemeasure
+import math
+
+
+pair_list_rep = {}
+tissue_list = []
+
+
+def func_dfs(Tree):
+	global pair_list_rep
+
+	ID = Tree.get_id()
+	dist = Tree.dist
+
+	if Tree.is_leaf():
+		return [ID]
+
+	else:
+		Tree_left = Tree.get_left()
+		Tree_right=  Tree.get_right()
+		list_left = func_dfs(Tree_left)
+		list_left = list_left[:]
+		list_right = func_dfs(Tree_right)
+		list_right = list_right[:]
+
+		pair_list_rep[ID] = (dist, list_left, list_right)
+
+		list_return = []
+		for element in list_left:
+			list_return.append(element)
+		for element in list_right:
+			list_return.append(element)
+
+		return list_return
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -111,6 +150,7 @@ if __name__ == '__main__':
 	'''
 
 
+
 	file = open("../GTEx_Data_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct_processed_2_gene_normalized_tissue_mean", 'r')
 	rep = {}
 	dimension = 0
@@ -136,20 +176,34 @@ if __name__ == '__main__':
 			array[i][j] = rpkm
 
 
+	##============ hierarchical clustering ============
 	Y = spatial.distance.pdist(array, 'euclidean')
-	Z = cluster.hierarchy.linkage(Y, method='single', metric='euclidean')
-	#cluster.hierarchy.dendrogram(Z, orientation='left')
+	Z = cluster.hierarchy.linkage(Y, method='single', metric='euclidean')	
+	##=========== get the pairwise distance matrix ===========
+	Tree = cluster.hierarchy.to_tree(Z)
+	func_dfs(Tree)
+
+	file = open("../tissue_hierarchy_normalized.txt", 'w')
+	for node in pair_list_rep:
+		dist = pair_list_rep[node][0]
+		list_left = pair_list_rep[node][1]
+		list_right = pair_list_rep[node][2]
+
+		for element1 in list_left:
+			for element2 in list_right:
+				print tissue_list[element1],
+				print " <--> ",
+				print tissue_list[element2],
+				print " : ",
+				print dist
+				file.write(tissue_list[element1] + '\t' + tissue_list[element2] + '\t' + str(dist) + '\n')
+	file.close()
+	##========================================================
+
+
 	R = cluster.hierarchy.dendrogram(Z, orientation='left', labels=tissue_list, color_threshold=0)
 
-	for i in range(len(tissue_list)):
-		print i,
-		print tissue_list[i]
 
-	for key in R:
-		print key
-		print R[key]
-
-
-
-	show()
+	plt.xlabel('distance')
+	plt.show()
 
